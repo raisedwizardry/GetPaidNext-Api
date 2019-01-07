7698fceb2cd2aa45915d81dfac91da1087fdff06
Eli Davis <eli.lyman.davis@gmail.com>
2019-01-06 23:55:07 -0500

    add unit tests to check Api Calculate Weekly and Biweekly methods return dates for given TestCase

--- GetPaidNext.Api.Tests/CalculateTests.cs
+++ GetPaidNext.Api.Tests/CalculateTests.cs
@@ -9,10 +9,52 @@ namespace GetPaidNext.Api.Tests
     public class CalculateTests
     {
         [Test]
-        public void PastDateReturnsEqualOrGreaterThanNowBiweeklyDate()
+        [TestCase(10)]
+        [TestCase(6)]
+        [TestCase(3)]
+        [TestCase(1)]
+        public void FutureDatesReturnEqualToSameDateBiweeklyDate(int date)
         {
             var systemUnderTest = new Calculate.Biweekly();
-            var given = systemUnderTest.NextPayDate(DateTime.Now.AddDays(-3).ToShortDateString());
+            var given = systemUnderTest.NextPayDate(DateTime.Now.AddDays(date).ToShortDateString());
+            var expected = DateTime.Today.AddDays(date);
+            Assert.That(DateTime.Parse(given), Is.EqualTo(expected));
+        }
+
+        [Test]
+        [TestCase(10)]
+        [TestCase(6)]
+        [TestCase(3)]
+        [TestCase(1)]
+        public void FutureDatesReturnEqualToSameDateWeeklyDate(int date)
+        {
+            var systemUnderTest = new Calculate.Weekly();
+            var given = systemUnderTest.NextPayDate(DateTime.Now.AddDays(date).ToShortDateString());
+            var expected = DateTime.Today.AddDays(date);
+            Assert.That(DateTime.Parse(given), Is.EqualTo(expected));
+        }
+
+        [Test]
+        [TestCase(-10)]
+        [TestCase(-6)]
+        [TestCase(-3)]
+        [TestCase(0)]
+        public void PastOrCurrentDatesReturnEqualOrGreaterThanSameDateBiweeklyDate(int date)
+        {
+            var systemUnderTest = new Calculate.Biweekly();
+            var given = systemUnderTest.NextPayDate(DateTime.Now.AddDays(date).ToShortDateString());
+            Assert.That(DateTime.Parse(given), Is.GreaterThanOrEqualTo(DateTime.Now));
+        }
+
+        [Test]
+        [TestCase(-10)]
+        [TestCase(-6)]
+        [TestCase(-3)]
+        [TestCase(0)]
+        public void PastOrCurrentDatesReturnEqualOrGreaterThanSameDateWeeklyDate(int date)
+        {
+            var systemUnderTest = new Calculate.Weekly();
+            var given = systemUnderTest.NextPayDate(DateTime.Now.AddDays(date).ToShortDateString());
             Assert.That(DateTime.Parse(given), Is.GreaterThanOrEqualTo(DateTime.Now));
         }
     }
