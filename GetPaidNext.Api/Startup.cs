c092413ac9904d4f76c32c54e40cb4dc2f6f8ca9
raisedwizardry <lifebloodasmr@gmail.com>
2019-02-24 01:00:46 -0500

    updated cors configuration for local development

--- GetPaidNext.Api/Startup.cs
+++ GetPaidNext.Api/Startup.cs
@@ -24,6 +24,7 @@ namespace GetPaidNext.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
@@ -33,6 +34,7 @@ namespace GetPaidNext.Api
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseCors(builder => builder.WithOrigins("http://localhost:4200"));
             }
 
             app.UseMvc();
